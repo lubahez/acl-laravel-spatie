725a61bf4993fedd6d858b4aabc9da3e7fa12730
Luis Ballesteros <33975183+lubahez@users.noreply.github.com>
2018-10-28 22:54:44 -0500

    Protección de rutas
    Agregando middlewares a los modulos de admin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
         ],
 
         'api' => [
@@ -58,5 +59,9 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,23 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
 
-Route::prefix('panel')->group(function (){
+Route::prefix('panel')->middleware(['auth'])->group(function (){
 
-	Route::get('usuarios/{id}/roles', 'AclController@usuarios_roles');
-	Route::get('usuarios/{id}/permisos', 'AclController@usuarios_permisos');
-	Route::put('usuarios/asignarroles/{id}', 'AclController@asignar_usuarios_roles');
-	Route::put('usuarios/asignarpermisos/{id}', 'AclController@asignar_usuarios_permisos');
+	Route::get('/home', 'HomeController@index')->name('home');
+	
+	Route::middleware(['role:admin'])->group(function () {
 
+		Route::get('usuarios/{id}/roles', 'AclController@usuarios_roles');
+		Route::get('usuarios/{id}/permisos', 'AclController@usuarios_permisos');
+		Route::put('usuarios/asignarroles/{id}', 'AclController@asignar_usuarios_roles');
+		Route::put('usuarios/asignarpermisos/{id}', 'AclController@asignar_usuarios_permisos');
 
-	Route::resource('usuarios', 'UsersController');
-	Route::get('roles/{id}/permisos', 'RolPermisosController@show');
-	Route::put('roles/asignar/{id}', 'RolPermisosController@asignar');
-	Route::resource('roles', 'RolesController');
-	Route::resource('permisos', 'PermisosController');
+		Route::resource('usuarios', 'UsersController');
+		Route::get('roles/{id}/permisos', 'RolPermisosController@show');
+		Route::put('roles/asignar/{id}', 'RolPermisosController@asignar');
+		Route::resource('roles', 'RolesController');
+		Route::resource('permisos', 'PermisosController');
 
+	});
 });
